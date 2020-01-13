0b1a3bd99cf2fec3599e43e402bbd741a3b162b5
Your Name <you@example.com>
2020-01-12 21:35:38 -0600

    1st day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+      textInput: "",
+      todos: [],
+      deleteTodo: true,
+      counter: 0
+    };
+  }
+  toggle = () => {
+    this.setState({ isClicked: !this.state.isClicked });
+  };
+  updateArray = () => {
+    const textInput = this.state.textInput;
+    const todos = this.state.todos;
+    let counter = this.state.counter
+    console.log(counter)
+    const newItem = {
+        textInput: textInput,
+        id: this.state.counter
+    }
+    todos.push(newItem);
+    counter= counter+1
+
+    this.setState({
+      todos: todos, 
+      counter: counter  
+    })
+
+    console.log(todos);
+  };
+  type = e => {
+    this.setState({ textInput: e.target.value });
+  };
+  
+  render() {
+    console.log("textInput", this.state.textInput);
+    console.log("todos", this.state.todos);
+
+    return (
+      <div>
+        <button onClick={this.updateArray}>Submit</button>
+        <input type="text" onChange={this.type}></input>
+        {this.state.todos.map(
+          (item, index) =>{
+            console.log(item)
+            
+          }
+        )}
+      </div>
+    );
+  }
 }
-
 export default App;
